1ff71f278fbdcbf55538487c0b5f6de3b4213aa1
Feng Qingyu <louisfqy@gmail.com>
2020-09-01 16:26:57 +0800

    update of Level0 to Level 3
    update of Level0 to Level 3

---
+++ src/main/java/Level0.java
@@ -0,0 +1,13 @@
+public class Level0 {
+
+        public static void main(String[] args){
+
+            System.out.println("____________________________________________________________\n");
+            System.out.println(" Hello! I'm Duke\n");
+            System.out.println(" What can I do for you?\n");
+            System.out.println("____________________________________________________________\n");
+            System.out.println(" Bye. Hope to see you again soon!\n");
+            System.out.println("____________________________________________________________\n");
+        }
+    }
+
