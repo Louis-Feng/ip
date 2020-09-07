ad9020aff5c9aaca7bd4f3401bc30f8f488cf956
Feng Qingyu <louisfqy@gmail.com>
2020-09-07 13:27:42 +0800

    Duke

---
+++ Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    public Task(String description,boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public Task completeTask(){
+        return new Task(this.description,true);
+    }
+    @Override
+    public String toString(){
+        return"["+this.getStatusIcon()+"] "+this.description;
+    }
+}
