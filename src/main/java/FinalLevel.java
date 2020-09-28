1a3d4822b3702f1fefec97728eeda9052c768336
Feng Qingyu <louisfqy@gmail.com>
2020-09-28 12:31:19 +0800

    Update

---
+++ src/main/java/FinalLevel.java
@@ -0,0 +1,213 @@
+
+/* import libraries*/
+import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.*;
+/** FinalLevel class which contains all the methods needed */
+public class FinalLevel {
+    public static String input; //input is for each statement input
+    public static ArrayList<Task> task = new ArrayList<Task>(); // create an arrayList to store tasks
+    public static File data = new File("D:\\ip\\src\\main\\data\\duke.txt"); //Create file object
+    public static FileOutputStream fos;
+    /* load data from txt file*/
+    static {
+        try {
+            fos = new FileOutputStream(data,true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Scanner in = new Scanner(System.in);
+    public static FileInputStream fis;
+
+    static {
+        try {
+            fis = new FileInputStream(data);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Scanner sc=new Scanner(fis);
+
+    public static void main(String[] args) throws IOException {
+        greet(); // call greet() method to greet
+        input = getNextStatement(); // get the input statement to execute.
+        /* mainly to execute input statements*/
+        while(!input.equals("bye")){ //if input is not "bye"
+            try {
+                /** print the list of tasks*/
+                if (input.equals("list")) {
+                    System.out.println("____________________________________________________________\n");
+                    /* iterate the task arrayList to print each task*/
+                    for (int i = 0; i < task.size(); i++) {
+                        if(task.get(i) instanceof Deadline){
+                            System.out.println((i + 1) + ":"  + (Deadline)task.get(i)); // print deadline task in its format
+                        }
+                        if(task.get(i) instanceof Event){
+                            System.out.println((i + 1) + ":"  + (Event)task.get(i)); // print event task in its format
+                        }
+                        if(task.get(i) instanceof Todo){
+                            System.out.println((i + 1) + ":"  + (Todo)task.get(i)); // print todo task in its format
+                        }
+
+                    }
+                    System.out.println("____________________________________________________________\n");
+                }
+                /** mark one task as done */
+                else if (input.startsWith("done")) {
+                    System.out.println("____________________________________________________________\n");
+                    int taskNumberCompleted = Integer.parseInt(input.replaceAll("\\D+", "")) - 1; //find the corresponding index of done task
+                    task.set(taskNumberCompleted, task.get(taskNumberCompleted).completeTask()); // set the corresponding task as done
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println(" "  + "[" + task.get(taskNumberCompleted).getStatusIcon() + "] " + task.get(taskNumberCompleted).description);
+                    System.out.println("____________________________________________________________\n");
+                }
+                /** delete one task */
+                else if (input.startsWith("delete")) {
+                    System.out.println("____________________________________________________________\n");
+                    System.out.println("Noted. I've removed this task:  ");
+                    int taskNumberdeleted = Integer.parseInt(input.replaceAll("\\D+", "")) - 1; //find the corresponding index of task to be deleted
+                    System.out.println(" " + "[" + task.get(taskNumberdeleted).getStatusIcon() + "] " + task.get(taskNumberdeleted).description);
+                    task.remove(taskNumberdeleted); //remove that task from arrayList
+                    System.out.println("Now you have " + task.size() + " tasks in the list. ");
+                    System.out.println("____________________________________________________________\n");}
+                /** to find tasks containing a certain keyword*/
+                else if (input.startsWith("find")){
+                    System.out.println("____________________________________________________________\n");
+                    System.out.println("Here are the matching tasks in your list:\n");
+                    String keyword = input.substring(4); // to get the keyword string
+                    int count = 1;
+                    /*iterate the task arrayList to find corresponding items*/
+                    for(int i=0;i< task.size();i++){
+                        if(task.get(i).description.contains(keyword)){
+                            System.out.println(count +": " + task.get(i));
+                            count ++;
+                        }
+                    }
+                    System.out.println("____________________________________________________________\n");
+                }
+                /** add a todo task*/
+                else if (input.startsWith("todo")) {
+                    Todo todo = new Todo(input); // create a todo object
+                    task.add(todo); // add it to the arrayList
+                    System.out.println("____________________________________________________________\n");
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println("   " + todo.toString());
+                    System.out.println("Now you have " + task.size() + " tasks in the list. ");
+                    System.out.println("____________________________________________________________\n");
+                }
+                /** add a deadline task*/
+                else if (input.startsWith("deadline")) {
+                    String descriptionOfDeadline;
+                    String by;
+                    descriptionOfDeadline = input.substring(9, (input.indexOf("/") - 1)); //get content of deadline description
+                    by = input.substring((input.indexOf("/") + 4)); //get time of event
+                    Deadline ddl = new Deadline(descriptionOfDeadline, by); //create a new deadline object with description and time
+                    task.add(ddl); // add to task arrayList
+                    System.out.println("____________________________________________________________\n");
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println("   " + ddl.toString());
+                    System.out.println("Now you have " + task.size() + " tasks in the list. ");
+                    System.out.println("____________________________________________________________\n");
+                }
+                /** add an event task*/
+                else if (input.startsWith("event")) {
+                    String descriptionOfevent;
+                    String at;
+                    descriptionOfevent = input.substring(6, (input.indexOf("/") - 1)); //get content of event description
+                    at = input.substring((input.indexOf("/") + 4)); //get time of event
+                    Event event = new Event(descriptionOfevent, at); //create a new event object with description and time
+                    task.add(event); // add to task arrayList
+                    System.out.println("____________________________________________________________\n");
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println("   " + event.toString());
+                    System.out.println("Now you have " + task.size() + " tasks in the list. ");
+                    System.out.println("____________________________________________________________\n");
+                }
+                else{ //dealing with undefined type of input
+                    System.out.println("____________________________________________________________\n");
+                    System.out.println("  OOPS!!! I'm sorry, but I don't know what that means :-(\n");
+                    System.out.println("____________________________________________________________\n");
+                }
+            /*exception handling of wrong input*/
+            }catch (Exception e){
+                if(input.equals("todo")) //wrong format of todo statement
+                {   System.out.println("____________________________________________________________\n");
+                    System.out.println(" OOPS!!! The description of a todo cannot be empty.");
+                    System.out.println("____________________________________________________________\n");
+                }
+                if(input.equals("deadline")) //wrong format of deadline statement
+                {   System.out.println("____________________________________________________________\n");
+                    System.out.println(" OOPS!!! The description of a deadline cannot be empty.");
+                    System.out.println("____________________________________________________________\n");
+                }
+                if(input.equals("event")) //wrong format of event statement
+                {   System.out.println("____________________________________________________________\n");
+                    System.out.println(" OOPS!!! The description of a event cannot be empty.");
+                    System.out.println("____________________________________________________________\n");
+                }
+
+
+            }
+
+            input = getNextStatement(); // get next input statement
+
+        }
+        write(task,data); //call write method to write list into txt file.
+        bye(); // call bye() method to say bye
+    }
+
+    /** method to write the list into a text file after "bye" statement*/
+    public static void write(ArrayList<Task> task, File data) throws IOException {
+        FileWriter writer = new FileWriter(data,true); //set append attribute to true so that it won't overwrite the file.
+        writer.write("\n"); // print a new line
+        for (int i = 0; i < task.size(); i++) {
+            if(task.get(i) instanceof Deadline){
+                writer.write("deadline " +(task.get(i)).toString() + "\n"); // print a deadline task
+            }
+            if(task.get(i) instanceof Event){
+                writer.write("event " + (task.get(i)).toString() + "\n"); // print an event task
+            }
+            if(task.get(i) instanceof Todo){
+                writer.write("todo " + (task.get(i)).toString() + "\n"); // print a todo task.
+            }
+
+        }
+        writer.close(); // finish writing
+    }
+    /** method to get next input statement from txt file or input stream*/
+    public static String getNextStatement(){
+        if(sc.hasNextLine()){ //if can read input statement from txt file
+            input=sc.nextLine();
+            if(input.startsWith("todo")){
+                input = "todo " + input.substring(12); // manipulate todo statement
+            }
+            if(input.startsWith("deadline")){
+                input = "deadline " + input.substring(16,(input.indexOf("(") )) + "/ by" + input.substring(input.indexOf(":")+2,input.indexOf(")"));//manipulate deadline statement
+            }
+            if(input.startsWith("event")){
+                input = "event " + input.substring(13,(input.indexOf("(") )) + "/ by" + input.substring(input.indexOf(":")+2,input.indexOf(")")); //manipulate event statement
+            }
+        }
+        else {
+            input=in.nextLine(); //get input statement from user input
+        }
+        return input;
+    }
+    /** method to greet*/
+    public static void greet(){
+        System.out.println("____________________________________________________________\n");
+        System.out.println(" Hello! I'm Duke\n");
+        System.out.println(" What can I do for you?\n");
+        System.out.println("____________________________________________________________\n");
+    }
+    /** method to say bye*/
+    public static void bye(){
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________\n");
+    }
+}
--- src/main/java/Level9.java
+++ src/main/java/Level9.java
@@ -1,24 +1,38 @@
 /** Level 9 class which contains all the methods needed */
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.*;
-import java.io.FileWriter;
+
 public class Level9 {
     public static void main(String[] args) throws IOException {
         String input;
         ArrayList<Task> task = new ArrayList<Task>();
+        File data = new File("D:\\ip\\src\\main\\data\\duke.txt");
+        FileOutputStream fos = new FileOutputStream(data,true);
+
+
         Scanner in = new Scanner(System.in);
-        FileInputStream fis=new FileInputStream("D:/ip/src/main/data/duke.txt");
+        FileInputStream fis=new FileInputStream(data);
         Scanner sc=new Scanner(fis);
-        FileWriter writer = new FileWriter("D:/ip/src/main/data/duke.txt");
+
+
         System.out.println("____________________________________________________________\n");
         System.out.println(" Hello! I'm Duke\n");
         System.out.println(" What can I do for you?\n");
         System.out.println("____________________________________________________________\n");
         if(sc.hasNextLine()){
-            input=sc.nextLine();}
+            input=sc.nextLine();
+            if(input.startsWith("todo")){
+                input = "todo " + input.substring(11);
+            }
+            if(input.startsWith("deadline")){
+                input = "deadline " + input.substring(16,(input.indexOf("(") )) + "/ by" + input.substring(input.indexOf(":")+2,input.indexOf(")"));
+            }
+            if(input.startsWith("event")){
+                input = "event " + input.substring(13,(input.indexOf("(") )) + "/ by" + input.substring(input.indexOf(":")+2,input.indexOf(")"));
+            }
+        }
         else {
             input=in.nextLine();
         }
@@ -44,7 +58,7 @@ public class Level9 {
                     int taskNumberCompleted = Integer.parseInt(input.replaceAll("\\D+", "")) - 1;
                     task.set(taskNumberCompleted, task.get(taskNumberCompleted).completeTask());
                     System.out.println(" Nice! I've marked this task as done:");
-                    System.out.println(" " + ":" + "[" + task.get(taskNumberCompleted).getStatusIcon() + "] " + task.get(taskNumberCompleted).description);
+                    System.out.println(" "  + "[" + task.get(taskNumberCompleted).getStatusIcon() + "] " + task.get(taskNumberCompleted).description);
                     System.out.println("____________________________________________________________\n");
                 } else if (input.startsWith("todo")) {
                     Todo todo = new Todo(input);
@@ -55,18 +69,14 @@ public class Level9 {
 
                     System.out.println("Now you have " + task.size() + " tasks in the list. ");
                     System.out.println("____________________________________________________________\n");
-
-
-
                 }
                 else if (input.startsWith("delete")) {
                     System.out.println("____________________________________________________________\n");
                     System.out.println("Noted. I've removed this task:  ");
                     int taskNumberdeleted = Integer.parseInt(input.replaceAll("\\D+", "")) - 1;
-                    //System.out.println(taskNumberdeleted);
+                    System.out.println(taskNumberdeleted);
                     System.out.println(" " + "[" + task.get(taskNumberdeleted).getStatusIcon() + "] " + task.get(taskNumberdeleted).description);
                     task.remove(taskNumberdeleted);
-
                     System.out.println("Now you have " + task.size() + " tasks in the list. ");
                     System.out.println("____________________________________________________________\n");}
                 else if (input.startsWith("find")){
@@ -145,18 +155,25 @@ public class Level9 {
             }
 
         }
-        for (int i = 0; i < task.size(); i++) {
-            if(task.get(i) instanceof Deadline){
-                writer.write("deadline " +task.get(i).description);}
-            if(task.get(i) instanceof Event){
-                writer.write("event " +task.get(i).description);}
-            if(task.get(i) instanceof Todo){
-                writer.write("todo " +task.get(i).description);}
-
-        }
-        writer.close();
+        write(task,data); //call write method to write list into txt file.
         System.out.println("____________________________________________________________\n");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________\n");
+    }
+
+    /** method to write the list into a text file after "bye" statement*/
+    public static void write(ArrayList<Task> task, File data) throws IOException {
+        FileWriter writer = new FileWriter(data,true); //set append attribute to true so that it won't overwrite the file.
+        writer.write("\n");
+        for (int i = 0; i < task.size(); i++) {
+            if(task.get(i) instanceof Deadline){
+                writer.write("deadline " +(task.get(i)).toString() + "\n");}
+            if(task.get(i) instanceof Event){
+                writer.write("event " + (task.get(i)).toString() + "\n");}
+            if(task.get(i) instanceof Todo){
+                writer.write("todo " + (task.get(i)).toString() + "\n");}
+
+        }
+        writer.close();
     }
 }
